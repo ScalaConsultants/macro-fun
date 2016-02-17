1f81aa679d027d8c4b6bdaba856a551b002277c1
Jakub Kozłowski <kubukoz@gmail.com>
2016-02-17 19:16:41 +0100

    mvp

--- src/main/scala/Macros.scala
+++ src/main/scala/Macros.scala
@@ -6,34 +6,6 @@ sealed trait Component {
   def toTree(implicit c: blackbox.Context): c.Tree
 }
 
-case class Negate(value: Component) extends Component {
-  override def toTree(implicit c: blackbox.Context): c.Tree = {
-    import c.universe._
-    q"-${value.toTree}"
-  }
-}
-
-case class Multiply(first: Component, second: Component) extends Component {
-  override def toTree(implicit c: blackbox.Context): c.Tree = {
-    import c.universe._
-    q"${first.toTree} * ${second.toTree}"
-  }
-}
-
-case class Add(first: Component, second: Component) extends Component {
-  override def toTree(implicit c: blackbox.Context): c.Tree = {
-    import c.universe._
-    q"${first.toTree} + ${second.toTree}"
-  }
-}
-
-case class Power(first: Component, second: Component) extends Component {
-  override def toTree(implicit c: blackbox.Context): c.Tree = {
-    import c.universe._
-    q"Math.pow(${first.toTree}, ${second.toTree})"
-  }
-}
-
 case class Variable(name: String) extends Component {
   override def toTree(implicit c: blackbox.Context): c.Tree = {
     import c.universe._
@@ -50,43 +22,27 @@ case class DoubleConstant(value: Double) extends Component {
 
 object Scalac {
 
-  def getComponent(tree: Trees#Tree)(implicit c: blackbox.Context): Component = {
+  private def getComponent(tree: Trees#Tree)(implicit c: blackbox.Context): Component = {
     import c.universe._
     tree match {
       case Ident(TermName(x)) => Variable(x)
       case Literal(Constant(a)) => DoubleConstant(a.toString.toDouble)
-      case q"-$x" => Negate(getComponent(x))
-      case q"+$x" => getComponent(x)
-      case q"$a + $b" => Add(getComponent(a), getComponent(b))
-      case q"$a - $b" => Add(getComponent(a), Negate(getComponent(b)))
-      case q"$a * $b" => Multiply(getComponent(a), getComponent(b))
-      case q"java.this.lang.Math.pow($a, $b)" => Power(getComponent(a), getComponent(b))
     }
   }
 
-  def extractComponents(tree: Trees#Tree)(implicit c: blackbox.Context): List[Component] = {
+  private def extractComponents(tree: Trees#Tree)(implicit c: blackbox.Context): List[Component] = {
     import c.universe._
     tree match {
-      case q"$nextTree + $arg" =>
-        getComponent(arg) :: extractComponents(nextTree)
-      case q"$nextTree - $arg" =>
-        Negate(getComponent(arg)) :: extractComponents(nextTree)
+      case q"$a + $b" => getComponent(a) :: getComponent(b) :: Nil
       case identOrLiteral => getComponent(identOrLiteral) :: Nil
     }
   }
 
-  def derive(comp: Component)(implicit c: blackbox.Context): c.Tree = {
+  private def derive(comp: Component)(implicit c: blackbox.Context): c.Tree = {
     import c.universe._
     comp match {
       case Variable(a) => q"1"
       case DoubleConstant(a) => q"0.0"
-      case Negate(a) => q"-${derive(a)}"
-      case Add(a, b) => q"${derive(a)} + ${derive(b)}"
-      case Multiply(a, b) =>
-        q"${a.toTree} * ${derive(b)} + ${derive(a)} * ${b.toTree}"
-      case Power(a@Variable(_), b@DoubleConstant(_)) =>
-        q"${a.toTree} * Math.pow(${a.toTree}, ${b.toTree} - 1)"
-      case expr => c.abort(c.enclosingPosition, s"Can't derive expression: $expr")
     }
   }
 
