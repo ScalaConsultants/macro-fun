c13db33d0b4b7f74f7fe4cbfce0e971cfc9ab467
Jakub Kozłowski <kubukoz@gmail.com>
2016-02-17 18:53:19 +0100

    enhancements, simplicifaction

--- src/main/scala/Macros.scala
+++ src/main/scala/Macros.scala
@@ -1,108 +1,155 @@
-import scala.language.dynamics
 import scala.language.experimental.macros
-import scala.reflect.macros.whitebox
+import scala.reflect.api.Trees
+import scala.reflect.macros.blackbox
 
-sealed trait Component
+sealed trait Component {
+  def toTree(implicit c: blackbox.Context): c.Tree
+}
 
-case class Negate(value: Component) extends Component
+case class Negate(value: Component) extends Component {
+  override def toTree(implicit c: blackbox.Context): c.Tree = {
+    import c.universe._
+    q"-${value.toTree}"
+  }
+}
 
-case class Multiply(first: Component, second: Component) extends Component
+case class Multiply(first: Component, second: Component) extends Component {
+  override def toTree(implicit c: blackbox.Context): c.Tree = {
+    import c.universe._
+    q"${first.toTree} * ${second.toTree}"
+  }
+}
 
-case class Power(first: Component, second: Component) extends Component
+case class Power(first: Component, second: Component) extends Component {
+  override def toTree(implicit c: blackbox.Context): c.Tree = {
+    import c.universe._
+    q"Math.pow(${first.toTree}, ${second.toTree})"
+  }
+}
 
-case class Add(first: Component, second: Component) extends Component
-
-case class Variable(name: String) extends Component
+case class Variable(name: String) extends Component {
+  override def toTree(implicit c: blackbox.Context): c.Tree = {
+    import c.universe._
+    Ident(TermName(name))
+  }
+}
 
 case class DoubleConstant(value: Double) extends Component {
-  override def toString: String = value.toString
+  override def toTree(implicit c: blackbox.Context): c.Tree = {
+    import c.universe._
+    Literal(Constant(value))
+  }
 }
 
 object Macros {
 
-  def integrate(f: Double => Double): Double => Double = macro integrateImpl
+  def derivative(f: Double => Double): Double => Double = macro derivativeImpl
 
-  def integrateImpl(c: whitebox.Context)(f: c.Expr[Double => Double]): c.Expr[Double => Double] = {
+  def derivativeImpl(c: blackbox.Context)(f: c.Expr[Double => Double]): c.Expr[Double => Double] = {
     import c.universe._
 
-    object SelectJavaMathPow {
-      def unapply(x: c.Tree): Boolean = x match {
-        case Select(Select(Select(This(TypeName("java")), TermName("lang")), TermName("Math")), TermName("pow")) => true
-        case _ => false
-      }
-    }
-
-    object BinaryOp {
-      def unapply(x: c.Tree): Option[(String, c.Tree, c.Tree)] = x match {
-        case Apply(Select(a, TermName(op)), List(b)) => Some((op, a, b))
-        case _ => None
-      }
-    }
-
-    def getComponent(tree: c.Tree): Component = tree match {
-      /*variable*/
+    def getComponent(tree: Trees#Tree): Component = tree match {
       case Ident(TermName(x)) => Variable(x)
-      /*constant*/
       case Literal(Constant(a)) => DoubleConstant(a.toString.toDouble)
-      /*multiplication*/
-      case BinaryOp("$times", a, b) =>
-        Multiply(getComponent(a), getComponent(b))
-      /*exponentation*/
-      case Apply(SelectJavaMathPow(), List(a, b)) =>
-        Power(getComponent(a), getComponent(b))
-      case Select(x, TermName("unary_$minus")) =>
-        Negate(getComponent(x))
-      case Select(x, TermName("unary_$plus")) =>
-        getComponent(x)
+      case q"-$x" => Negate(getComponent(x))
+      case q"+$x" => getComponent(x)
+      case q"$a * $b" => Multiply(getComponent(a), getComponent(b))
+      case q"java.this.lang.Math.pow($a, $b)" => Power(getComponent(a), getComponent(b))
     }
 
-    def extractComponents(tree: c.Tree): List[Component] = tree match {
-      /*addition*/
-      case BinaryOp("$plus", arg, nextTree) =>
-        getComponent(nextTree) :: extractComponents(arg)
-      /*substraction*/
-      case BinaryOp("$minus", arg, nextTree) =>
-        Negate(getComponent(nextTree)) :: extractComponents(arg)
-      /*single component*/
+    def extractComponents(tree: Trees#Tree): List[Component] = tree match {
+      case q"$nextTree + $arg" =>
+        getComponent(arg) :: extractComponents(nextTree)
+      case q"$nextTree - $arg" =>
+        Negate(getComponent(arg)) :: extractComponents(nextTree)
       case identOrLiteral => getComponent(identOrLiteral) :: Nil
     }
 
-    val Function(List(ValDef(_, TermName(argName), _, _)), funcBody) = f.tree
+    val Function(List(ValDef(_, name, _, _)), funcBody) = f.tree
 
     val components = extractComponents(funcBody)
 
-    /*this part is actually unnecessary*/
-    println("is function valid? " + components.forall {
-      case Variable(name) if name != argName => false
-      case _ => true
-    })
-
-    println("Body: " + components)
-
-    def getTreeOf(comp: Component): c.Tree = comp match {
-      case Variable(a) => Ident(TermName(a))
-      case DoubleConstant(a) => q"$a"
-      case Multiply(a, b) => q"${getTreeOf(a)} * ${getTreeOf(b)}"
-      case Power(a, b) => q"Math.pow(${getTreeOf(a)}, ${getTreeOf(b)})"
-      case Add(a, b) => q"${getTreeOf(a)} + ${getTreeOf(b)}"
-      case Negate(a) => q"-${getTreeOf(a)}"
+    def derive(comp: Component): c.Tree = comp match {
+      case Variable(a) => q"1"
+      case DoubleConstant(a) => q"0.0"
+      case Negate(a) => q"-${derive(a)}"
+      case Multiply(a, b) =>
+        q"${a.toTree(c)} * ${derive(b)} + ${derive(a)} * ${b.toTree(c)}"
+      case Power(a@Variable(_), b@DoubleConstant(_)) =>
+        q"${a.toTree(c)} * Math.pow(${a.toTree(c)}, ${b.toTree(c)} - 1)"
+      case expr => c.abort(c.enclosingPosition, s"Can't derive expression: $expr")
     }
 
-    val transformedComponents = components.map(comp => getTreeOf(comp)).reduce((a, b) => q"$a + $b")
+    val transformedComponents = components.map(comp => derive(comp)).reduce((a, b) => q"$a + $b")
 
-    val z = q"(x: Double) => $transformedComponents"
-
+    val z = q"($name: Double) => $transformedComponents"
+    println(show(z))
     c.Expr(z)
+  }
+
+  def hello = macro helloImpl
+
+  def helloImpl(c: blackbox.Context): c.Expr[Unit] = {
+    import c.universe._
+
+    /*reify {
+      println("hello!")
+    }
+
+    c.Expr {
+      Apply(Ident(TermName("println")), List(Literal(Constant("hello!"))))
+    }*/
+
+    c.Expr(q"""println("hello!")""")
+  }
+
+  def hello2(s: String) = macro hello2Impl
+
+  def hello2Impl(c: blackbox.Context)(s: c.Expr[String]): c.Expr[Unit] = {
+    import c.universe._
+
+    reify {
+      println(s"hello ${s.splice}!")
+    }
+
+    c.Expr {
+      Apply(
+        Ident(TermName("println")),
+        List(
+          Apply(
+            Select(
+              Apply(
+                Select(
+                  Literal(Constant("hello ")),
+                  TermName("$plus")
+                ),
+                List(
+                  s.tree
+                )
+              ),
+              TermName("$plus")
+            ),
+            List(
+              Literal(Constant("!"))
+            )
+          )
+        )
+      )
+    }
+
+    c.Expr(q"""println("hello " + ${s.tree} + "!")""")
   }
 
   def debug[T](param: T) = macro debugImpl[T]
 
-  def debugImpl[T](c: whitebox.Context)(param: c.Expr[T]): c.Expr[Unit] = {
+  def debugImpl[T](c: blackbox.Context)(param: c.Expr[T]): c.Expr[Unit] = {
     import c.universe._
 
-    val name = param.tree.toString
-    val value = param.tree
+    //    c.Expr(q"""println(${showRaw(param.tree)} + " = " + $param)""")
+    val z = c.Expr[String](Literal(Constant(showRaw(param.tree))))
 
-    c.Expr(q"""println($name + " = " +  $value)""")
+    reify {
+      println(z.splice + " = " + param.splice)
+    }
   }
 }
